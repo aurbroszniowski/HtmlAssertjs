bf780379dddb99f73372286d485ca0a4878223f6
jsoftbiz@gmail.com <pamela55>
2013-12-10 11:57:45 +0100

    pushing DSL into array for server-side treatment

--- src/HtmlAssert.js
+++ src/HtmlAssert.js
@@ -1,15 +1,52 @@
+//-------------- Tag class
+var Tag = function(tag, attributes) {
+    this.tag = tag;
+    this.attributes = attributes;
+}
+var Tagp = Tag.prototype;
+Tagp.getTag = function () {
+   return this.tag;
+}
+Tagp.getAttributes = function () {
+   return this.attributes;
+}
+Tagp.toString = function () {
+    var out = this.tag + '[';
+    for (var i = 0; i < this.attributes.length; i++) {
+        out += this.attributes[i];
+        if (i < (this.attributes.length - 1)) {
+            out += ', ';
+        }
+    }
+    out += ']';
+    return out;
+}
+
+
+//-------------- HtmlAssert class
 var HtmlAssert = function(html) {
     this.tagsList = new Array();
     this.html = html;
 };
 
+HtmlAssert.it = function (title, currentTest) {
+    console.info(title);
+    currentTest();
+};
 
 var Tp = HtmlAssert.prototype;
 Tp.toString = function () {
-    var ret="";
+    var ret = "";
     for (var i=0; i < this.tagsList.length; i++) {
-        ret += this.tagsList[i] + "\n";
+        ret += this.tagsList[i].toString() + "\n";
     }
     return ret;
 };
-Tp.div = function(attributes) { this.tagsList.push('div'); return this; };
+Tp.div = function() {
+    if (arguments.length > 0) {
+            this.tagsList.push(new Tag('div', arguments));
+    } else {
+        this.tagsList.push(new Tag('div', new Array()));
+    }
+    return this;
+};
--- test/HtmlAssertTest.js
+++ test/HtmlAssertTest.js
@@ -1,9 +1,10 @@
-describe("A suite", function() {
-    it("contains spec with an expectation", function() {
-        var html = "<dZv><div></div></dZv>";
-        var htmlAssert = new HtmlAssert(html);
-
-        console.info(">>> >> "+htmlAssert.div().div().toString());
+describe("A suite", function () {
+    it("contains spec with an expectation", function () {
+        HtmlAssert.it("should contain those divs", function () {
+            var html = "<dZv><div><div id=\"someid\"></div></div></dZv>";
+            var htmlAssert = new HtmlAssert(html);
+            console.info(">>> >> \n" + htmlAssert.div().div("id", "someid").toString());
+        });
     });
 });
 
