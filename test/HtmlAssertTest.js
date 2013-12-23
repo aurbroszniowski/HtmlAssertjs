2a771319a1c6416be53655a4efb1d8eb6faecd85
jsoftbiz <jsoftbiz@gmail.com>
2013-12-23 13:03:59 +0100

    Adding all html tags

--- test/HtmlAssertTest.js
+++ test/HtmlAssertTest.js
@@ -1,16 +1,16 @@
-//describe("A suite", function () {
-//    it("contains spec with an expectation", function () {
-//        var html = "<dZv><div><div id=\"someid\"></div></div></dZv>";
-//        var htmlAssert = new HtmlAssert(html);
-//        HtmlAssert.it("should contain those divs", function() { return htmlAssert.div().p("class", "someid", "id", "someied")});
-//    });
-//});
-
-describe("A suite", function () {
-    it("contains spec with an expectation", function () {
+describe("HtmlAssert core", function () {
+    it("test a passing div", function () {
         var html = "<dZv><p><div class=\"someclass\" id=\"someid\"></div></p></dZv>";
         var htmlAssert = new HtmlAssert(html);
         HtmlAssert.it("should contain those divs", function() { return htmlAssert.p().div("id", "someid", "class", "someclass")});
     });
+
+    it("test HTML on Multiple Lines", function () {
+        var html = " <div>           <td title=\"en-gb\"\n" +
+            "                style=\"166px;\">en-gb</td></div>\n";
+        var htmlAssert = new HtmlAssert(html);
+        HtmlAssert.it("should contain this td", function() { return htmlAssert.td("title", "en-gb", "style", "166px;")});
+    });
+
 });
 
